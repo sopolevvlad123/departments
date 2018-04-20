34e1a01e2ba6818f6d2fb312dc2873b515fe2299
Vladislav <v.sopolev@aimprosoft.com>
2018-04-20 15:25:32 +0300

    Initial commit

---
+++ src/main/java/com/servlet/FrontController.java
@@ -0,0 +1,10 @@
+package com.servlet;
+
+public class FrontController
+cd existing_folder
+        git init
+        git remote add origin https://git.aimprosoft.com/education/v.sopolev.git
+        git add .
+        git commit -m "Initial commit"
+        git push -u origin master
+}
