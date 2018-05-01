6a0929e9302451f29c18dbc998b8c2f5abb73e9d
Vladislav <v.sopolev@aimprosoft.com>
2018-05-01 15:54:42 +0300

    Hiber generic sandbox

---
+++ src/main/java/com/dao/IGenericDAO.java
@@ -0,0 +1,19 @@
+package com.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IGenericDAO<T> {
+
+    T get(Integer id) throws SQLException;
+
+    void saveOrUpdate(T t) throws SQLException;
+
+    List<T> getAll() throws SQLException;
+
+    List<T> getByParam(Integer id) throws SQLException;
+
+    void delete(Integer employeeId) throws SQLException;
+
+    boolean checkUnique(T t) throws SQLException;
+}
