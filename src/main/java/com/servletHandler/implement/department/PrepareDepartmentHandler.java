90d69f59b5c4763592716dc73073da6d0253343a
Vladislav <v.sopolev@aimprosoft.com>
2018-05-03 19:07:48 +0300

    for stas v2

---
+++ src/main/java/com/servletHandler/implement/department/PrepareDepartmentHandler.java
@@ -0,0 +1,36 @@
+package com.servletHandler.implement.department;
+
+import com.bean.Department;
+import com.exception.AppException;
+import com.exception.ServiceException;
+import com.servletHandler.ServletHandler;
+import com.utils.RequestDataParser;
+import org.apache.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.utils.ServletHandlerConstants.SAVE_DEPARTMENT_PAGE;
+
+public class PrepareDepartmentHandler implements ServletHandler {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, AppException {
+        String departmentId = request.getParameter("departmentId");
+        if (RequestDataParser.isIDValid(departmentId)) {
+            try {
+                Department department = departmentService.getDepartment(Integer.parseInt(departmentId));
+                request.setAttribute("departmentName", department.getDepartmentName());
+            } catch (ServiceException e) {
+                Logger log = (Logger) request.getSession().getServletContext().getAttribute("appLogger");
+                log.error(e);
+                throw new AppException("Fail to get department at application layer", e);
+            }
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(SAVE_DEPARTMENT_PAGE);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/java/com/servletHandler/implement/emploee/PrepareEmployeeHandler.java
@@ -0,0 +1,42 @@
+package com.servletHandler.implement.emploee;
+
+import com.bean.Employee;
+import com.exception.AppException;
+import com.exception.ServiceException;
+import com.servletHandler.ServletHandler;
+import com.utils.RequestDataParser;
+import org.apache.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.utils.ServletHandlerConstants.SAVE_EMPLOYEE_PAGE;
+
+public class PrepareEmployeeHandler implements ServletHandler {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, AppException {
+        String employeeId = request.getParameter("employeeId");
+        if (RequestDataParser.isIDValid(employeeId)) {
+            try {
+                Employee employee = employeeService.getEmployee(Integer.parseInt(employeeId));
+                request.setAttribute("firstName", employee.getFirstName());
+                request.setAttribute("lastName", employee.getLastName());
+                request.setAttribute("email", employee.getEmail());
+                request.setAttribute("salary", employee.getSalary());
+                request.setAttribute("hireDate", employee.getHireDate());
+                request.setAttribute("departmentId", employee.getDepartmentId());
+            } catch (ServiceException e) {
+                Logger log = (Logger) request.getSession().getServletContext().getAttribute("appLogger");
+                log.error(e);
+                throw new AppException("Fail to get employee at application layer", e);
+            }
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(SAVE_EMPLOYEE_PAGE);
+        dispatcher.forward(request, response);
+    }
+}
