4319001415820c71af176c26d48546b1050c81b4
Vladislav <v.sopolev@aimprosoft.com>
2018-05-18 16:23:31 +0300

    some stop

--- src/main/java/com/controller/ExceptionController.java
+++ src/main/java/com/controller/ExceptionController.java
@@ -1,10 +1,14 @@
 package com.controller;
 
 import com.exception.AppException;
+import com.exception.Page404Exception;
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -25,4 +29,14 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
         model.addAttribute("error",new Exception("something goes wrong"));
         return "errorPage";
     }
+
+  /*  @ExceptionHandler(value = NoHandlerFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String fortyExceptionHandle(Throwable throwable, Model model) {
+        throwable.printStackTrace();
+        logger.error(throwable);
+        System.out.println("hello");
+        model.addAttribute("error", new Exception("forti four"));
+        return "errorPage";
+    }*/
 }
--- src/main/java/com/utils/Initializer.java
+++ src/main/java/com/utils/Initializer.java
@@ -1,5 +1,6 @@
 package com.utils;
 
+import com.controller.ExceptionController;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -13,9 +14,11 @@ public class Initializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext){
         AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
         dispatcherContext.register(SpringConfig.class);
+        dispatcherContext.register(ExceptionController.class);
         String DISPATCHER_NAME = "springDispatcher";
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet(DISPATCHER_NAME, new DispatcherServlet(dispatcherContext));
         dispatcher.setLoadOnStartup(1);
+        dispatcher.setInitParameter("throwExceptionIfNoHandlerFound", "true");
         dispatcher.addMapping("/");
     }
 
